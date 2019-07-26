93f80bc8647faa1a4a07a71672dabea7fb5a38b5
Trevor Fairbank <Trevorfairbank92@gmail.com>
2019-07-26 13:46:37 -0700

    track

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -13,7 +13,7 @@ class Navbar extends Component {
             return (
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <Link className="nav-link" to="/profile">Profile</Link>
+                        <Link className="nav-link" to="/profile">Templates</Link>
                     </li>
                     <li className="nav-item">
                         {/* this is not using the Link component to logout or user and then refresh the application to the start */}
@@ -37,9 +37,9 @@ class Navbar extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
                 <div className="container">
-                    <Link className="navbar-brand" to="/">React JWT App</Link>
+                    <Link className="navbar-brand" to="/">Codr</Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,88 @@
 import React from 'react';
+import logo from '../pages/codr-logo.png'
 
- /* Add in Logo here and slides down to about section. */
+/* Add in Logo here and slides down to about section. */
 
-function Home(){
-    return <div>
-        <h1>
-            <a href="./login">LOGIN</a>
-        </h1>
-    </div>
+function Home() {
+    return (
+        <div>
+            <header className="masthead text-center text-white">
+                <div className="masthead-content">
+                    <div className="container">
+                        <img src={logo} alt="logo" width="100px" height="100px" />
+                        <h1 className="masthead-heading mb-0">One Page Wonder</h1>
+                        <h2 className="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
+                        <a href="google.com" className="btn btn-primary btn-xl rounded-pill mt-5">Learn More</a>
+                    </div>
+                </div>
+                <div className="bg-circle-1 bg-circle"></div>
+                <div className="bg-circle-2 bg-circle"></div>
+                <div className="bg-circle-3 bg-circle"></div>
+                <div className="bg-circle-4 bg-circle"></div>
+            </header>
+
+            <section>
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-6 order-lg-2">
+                            <div className="p-5">
+                                <img className="img-fluid rounded-circle" src="img/01.jpg" alt="" />
+                            </div>
+                        </div>
+                        <div className="col-lg-6 order-lg-1">
+                            <div className="p-5">
+                                <h2 className="display-4">For those about to rock...</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-6">
+                            <div className="p-5">
+                                <img className="img-fluid rounded-circle" src="img/02.jpg" alt="" />
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="p-5">
+                                <h2 className="display-4">We salute you!</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-6 order-lg-2">
+                            <div className="p-5">
+                                <img className="img-fluid rounded-circle" src="img/03.jpg" alt="" />
+                            </div>
+                        </div>
+                        <div className="col-lg-6 order-lg-1">
+                            <div className="p-5">
+                                <h2 className="display-4">Let there be rock!</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <footer className="py-5 bg-black">
+                <div className="container">
+                    <p className="m-0 text-center text-white small">Copyright &copy; Your Website 2019</p>
+                </div>
+            </footer>
+
+        </div>
+    )
 }
 
 

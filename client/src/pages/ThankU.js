7eb1e247c682f3fe34e1b8f50221a16cdaa3d388
soheil behzad <soheil_dark999@yahoo.com>
2019-07-29 14:07:25 -0700

    new pull

---
+++ client/src/pages/ThankU.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import logo from '../pages/codr-logo.png'
+import Jumbotron from "../components/Jumbotron/Jumbotron"
+
+function ThankU () {
+
+    return (
+        <div>
+            <Jumbotron>
+
+            <button type="button" class="btn btn-primary">Learn To Deploy</button>
+            </Jumbotron>
+
+        </div>
+
+
+    )
+
+
+}
+
+export default ThankU;

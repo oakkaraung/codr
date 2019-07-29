63d4f09aba93864604c35d0a33d02c6be5bdd13a
soheil behzad <soheil_dark999@yahoo.com>
2019-07-29 12:40:46 -0700

    new pull monday 7/29

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -41,33 +41,6 @@ class Profile extends Component {
               </div>
             </Jumbotron>
             <OnClickTemp />
-            {/* <form>
-                <Input name="title" placeholder="Title (required)" onChange= {this.handleInputChange} value={this.state.title} />
-                <Input name="author" placeholder="Author (required)" onChange= {this.handleInputChange} value={this.state.author}/>
-                <TextArea name="synopsis" placeholder="Synopsis (Optional)" onChange= {this.handleInputChange} value={this.state.synopsis}/>
-                <FormBtn onClick={this.handleSubmit}>Submit Book</FormBtn>
-              </form> */}
-            {/* </Col>
-            <Col size="md-6 sm-12">
-              <Jumbotron>
-                <h1>Books On My List</h1>
-              </Jumbotron>
-              {this.state.books.length ? (
-                <List>
-                  {this.state.books.map(book => (
-                    <ListItem key={book._id}>
-                      <a href={"/books/" + book._id}>
-                        <strong>
-                          {book.title} by {book.author}
-                        </strong>
-                      </a>
-                      <DeleteBtn onClick={() => this.handleDelete(book._id)}/>
-                    </ListItem>
-                  ))}
-                </List>
-              ) : (
-                <h3>No Results to Display</h3>
-              )} */}
           </Col>
         </Row>
       </Container>
